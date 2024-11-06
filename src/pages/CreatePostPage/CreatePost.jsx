913faee81c840d711b8f0705f19460cc0a05ee82
taesong04 <taesong04@gmail.com>
2024-11-06 22:36:06 +0900

    classname

--- src/pages/CreatePostPage/CreatePost.jsx
+++ src/pages/CreatePostPage/CreatePost.jsx
@@ -8,21 +8,21 @@ const CreatePost = () => {
     };
 
     return (
-        <main>
+        <main className="create-post-main">
             <div className="container">
                 <h1 className="title">게시판 작성</h1>
                 <div className="button-container">
                     <button type="button" className="upload-button">사진</button>
-                    <button type="submit" onClick={handleSubmit}>등록</button>
+                    <button type="submit" className="submit-button" onClick={handleSubmit}>등록</button>
                 </div>
                 <form className="post-form" onSubmit={handleSubmit}>
                     <div className="form-group">
-                        <label htmlFor="post-title">제목:</label>
-                        <input type="text" id="post-title" name="post-title" required />
+                        <label htmlFor="post-title" className="form-label">제목:</label>
+                        <input type="text" id="post-title" name="post-title" className="form-input" required />
                     </div>
                     <div className="form-group">
-                        <label htmlFor="post-content">내용:</label>
-                        <textarea id="post-content" name="post-content" rows="10" required></textarea>
+                        <label htmlFor="post-content" className="form-label">내용:</label>
+                        <textarea id="post-content" name="post-content" className="form-textarea" rows="10" required></textarea>
                     </div>
                 </form>
             </div>
